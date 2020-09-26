4c0497228134e8654ea28e6301e2917f8f6003e1
Philipp Eder <philipp.eder@posteo.net>
2020-09-26 13:38:52 +0200

    add check for vimx and rg in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,12 @@
-alias vim='vim -c "let g:tty='\'(tty)\''"'
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --no-ignore-vcs'
+if type -q vimx
+    alias vim='vimx -c "let g:tty='\'(tty)\''"'
+else
+    alias vim='vim -c "let g:tty='\'(tty)\''"'
+end
+
+if type -q rg
+    export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --no-ignore-vcs'
+end
 
 if test -e $HOME/.config/fish/specific.fish
     source $HOME/.config/fish/specific.fish
