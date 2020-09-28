c250f9980bd93357c564560ff497c4a1857f8334
Philipp Eder <philipp.eder@posteo.net>
2020-09-28 08:57:20 +0200

    add battery to statusbar

--- bin/dwm_statusbar.sh
+++ bin/dwm_statusbar.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
 
 while true; do
+    BATTERY=""
+    for bat in /sys/class/power_supply/BAT* ; do
+        name=`basename $bat | sed 's/BAT//g'`
+        #status=`cat $bat/status`
+        now=`cat $bat/energy_now`
+        full=`cat $bat/energy_full`
+        cycle_count=`cat $bat/cycle_count`
+        BATTERY="$BATTERY ($name: ♽ $cycle_count $now/$full)" 
+    done
+    BATTERY="| 🔋 $BATTERY"
     TIME=`date '+%a %Y-%m-%d %H:%M'`
-	xsetroot -name "| 📅︎ $TIME |"
+	xsetroot -name "$BATTERY | 📅︎ $TIME |"
 	sleep 55s
 done &
