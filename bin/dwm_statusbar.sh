d66d2434b16e3a537965305c4f2ca3a1cc84b080
Philipp Eder <philipp.eder@posteo.net>
2020-12-29 13:17:20 +0100

    add small programm to read the battery percentage
    since I switched back to debian I cannot use acpiconf anymore.

--- bin/dwm_statusbar.sh
+++ bin/dwm_statusbar.sh
@@ -2,11 +2,6 @@
 
 
 while true; do
-    BAT0="0: `acpiconf -i 0 | grep "Remaining capacity" | sed 's/Remaining capacity:[[:space:]]*//'`"
-    BAT1="1: `acpiconf -i 1 | grep "Remaining capacity" | sed 's/Remaining capacity:[[:space:]]*//'`"
-    BATTERY="$BAT0 ♽ $BAT1" 
-    BATTERY="| $BATTERY"
-    TIME=`date '+%a %Y-%m-%d %H:%M'`
-	xsetroot -name "$BATTERY | $TIME |"
-	sleep 55
-done &
+	xsetroot -name "`dwm-bat-time`"
+	sleep 1 
+done & 
