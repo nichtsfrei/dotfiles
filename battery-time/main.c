521e514ba7fc2949d7fb482163dad1ae6243ff15
Philipp Eder <philipp.eder@greenbone.net>
2021-01-08 09:09:44 +0100

    on some machines it's charge_now instead of energy_now

--- battery-time/main.c
+++ battery-time/main.c
@@ -22,12 +22,20 @@ int main(){
 		snprintf(energy_now_path, 40, "/sys/class/power_supply/BAT%d/energy_now", i);
 		energy_now = read_from_file(energy_now_path);
 		if (energy_now < 0){
-			break;
+			snprintf(energy_now_path, 40, "/sys/class/power_supply/BAT%d/charge_now", i);
+			energy_now = read_from_file(energy_now_path);
+			if (energy_now < 0){
+				break;
+			}
 		}
 		snprintf(energy_full_path, 41, "/sys/class/power_supply/BAT%d/energy_full", i);
 		energy_full = read_from_file(energy_full_path);
 		if (energy_full < 0){
-			break;
+			snprintf(energy_full_path, 41, "/sys/class/power_supply/BAT%d/charge_full", i);
+			energy_full = read_from_file(energy_full_path);
+			if (energy_full < 0){
+				break;
+			}
 		}
 		printf("| BAT%d: %0.2f ", i, (float) energy_now / (float) energy_full);
 	}
