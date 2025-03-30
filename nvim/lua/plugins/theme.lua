dc99c18d58d21c08d5fe42e7a635debf22a409b9
Philipp Eder <philipp.eder@posteo.net>
2025-03-30 11:19:43 +0200

    Back again ...

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,8 @@
+return {
+    "NLKNguyen/papercolor-theme",
+    branch = "master",
+    config = function()
+        vim.opt.background = "light"
+        vim.cmd("colorscheme PaperColor")
+    end,
+}
