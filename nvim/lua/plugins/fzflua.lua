6592bd3075de7b1b2d5419959a4e690d199566e7
Philipp Eder <philipp.eder@posteo.net>
2025-03-31 18:25:36 +0200

    Some shortcut changes

--- nvim/lua/plugins/fzflua.lua
+++ nvim/lua/plugins/fzflua.lua
@@ -5,21 +5,27 @@ return {
     -- or if using mini.icons/mini.nvim
     dependencies = { "echasnovski/mini.icons" },
     opts = {},
-    keys={
-        { 
-            "<leader>ff",
-            function() require('fzf-lua').files() end,
-            desc="Find Files in project directory"
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("fzf-lua").files()
+            end,
+            desc = "Find Files in project directory",
         },
-        { 
-            "<leader>fg",
-            function() require('fzf-lua').live_grep() end,
-            desc="Find by grepping in project directory"
+        {
+            "<leader>/",
+            function()
+                require("fzf-lua").live_grep()
+            end,
+            desc = "Find by grepping in project directory",
         },
-        { 
+        {
             "<leader>fc",
-            function() require('fzf-lua').files({cwd=vim.fn.stdpath("config")}) end,
-            desc="Find in neovim configuration"
+            function()
+                require("fzf-lua").files({ cwd = vim.fn.stdpath("config") })
+            end,
+            desc = "Find in neovim configuration",
         },
         {
             "<leader>fh",
@@ -84,12 +90,12 @@ return {
             end,
             desc = "[,] Find existing buffers",
         },
-        {
-            "<leader>/",
-            function()
-                require("fzf-lua").lgrep_curbuf()
-            end,
-            desc = "[/] Live grep the current buffer",
-        },
-    }
+        -- {
+        --     "<leader>/",
+        --     function()
+        --         require("fzf-lua").lgrep_curbuf()
+        --     end,
+        --     desc = "[/] Live grep the current buffer",
+        -- },
+    },
 }
